c1fa9787563e67724b71384d0a4c657543a3dd3e
Kevin Todd <ktodd@MP-050710N1.local>
2013-06-17 12:02:58 -0500

    added Log

--- GitUseExample/AppDelegate.m
+++ GitUseExample/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    NSLog(@"The application did indeed finish launching.");
     // Override point for customization after application launch.
     return YES;
 }
